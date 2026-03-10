44cdb37407b3f8b5da918b8074821d357eb382ba
Cathlyn Clyde Resmeros <cathlyn.jandog@gmail.com>
2026-03-10 18:16:31 +0800

    Add exercise 2.12 solution

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -29,14 +29,21 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return
     }
-
-    setPersons(persons.concat({
+    
+    const newPerson = {
       name: newName,
       number: newNumber,
       id: persons.length + 1
-    }))
-    setNewName('')
-    setNewNumber('')
+    }
+
+    axios
+      .post('http://localhost:3001/persons', newPerson)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
+
   }
 
   const handleChange = (event) => {
