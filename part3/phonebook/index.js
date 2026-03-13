31fb08843ca25cd49ee3a3242016b4bab070c4dd
Cathlyn Clyde Resmeros <cathlyn.jandog@gmail.com>
2026-03-13 22:12:48 +0800

    Add exercise 3.5 solution

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -57,6 +57,29 @@ app.get('/info', (request, response) => {
     response.send(html)
 })
 
+const generateId = () => {
+    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name and number are required'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
