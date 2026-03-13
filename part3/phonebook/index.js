83108b77e3bbc1c18d3cb9dcebd35712fcd898d2
Cathlyn Clyde Resmeros <cathlyn.jandog@gmail.com>
2026-03-13 17:45:50 +0800

    Add exercise 3.2 solution

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -30,6 +30,14 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const html = `
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    `
+    response.send(html)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
