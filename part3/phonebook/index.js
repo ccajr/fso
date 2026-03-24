3a1358a4a91be0c855e87052fac9831f33a66fc8
Cathlyn Clyde Resmeros <cathlyn.jandog@gmail.com>
2026-03-24 16:04:09 +0800

    Add exercise 3.16 solution

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -39,11 +39,12 @@ app.get('/api/persons/:id', (request, response) => {
     
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(204).end()
         })
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -53,10 +54,6 @@ app.get('/info', (request, response) => {
     `
     response.send(html)
 })
-
-const generateId = () => {
-    return String(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER))
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -91,6 +88,18 @@ app.post('/api/persons', (request, response) => {
     })
 
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
