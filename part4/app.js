a28e79fb93f6d6ffa1dd708aa3ca1dba77cd8327
Cathlyn Clyde Resmeros <cathlyn.jandog@gmail.com>
2026-03-31 15:35:29 +0800

    Add exercise 4.2 solution

---
+++ part4/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { family: 4 })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
