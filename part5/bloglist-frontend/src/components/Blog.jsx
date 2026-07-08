f1d328060637087da108cee4f5fa0d2d978e9775
Cathlyn Clyde Resmeros <cathlyn.jandog@gmail.com>
2026-07-08 15:02:57 +0800

    Add exercise 5.31 solution

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,3 +1,4 @@
+import { Button, Card, CardActionArea, CardContent, CardActions, Typography } from '@mui/material'
 const Blog = ({ blog, updateBlog, user, deleteBlog }) => {
   if (!blog) {
     return null
@@ -22,18 +23,19 @@ const Blog = ({ blog, updateBlog, user, deleteBlog }) => {
   }
 
   return (
-    <div>
-      <h2>{blog.author}: {blog.title}</h2>
-      <div>
-        <a className='url' target="_blank" href={blog.url}>{blog.url}</a>
-        <div>
-          likes {blog.likes}
-          {user && <button onClick={handleLike}>like</button>}
-        </div>
-        <div>Added by {blog.user.name}</div>
-        {isCreator && <button onClick={handleRemove}>remove</button>}
-      </div>
-    </div>
+    <Card>
+      <CardContent>
+        <Typography variant='h5' component='div'>{blog.title}</Typography>
+        <Typography variant='body1' sx={{ color: 'text.secondary' }}>by {blog.author}</Typography>
+        <Typography variant='body2' component='a' className='url' target="_blank" href={blog.url} >{blog.url}</Typography>
+        <Typography variant='body2' sx={{ color: 'text.secondary' }}>Added by {blog.user.name}</Typography>
+      </CardContent>
+      <CardActions sx={{ marginLeft: 8 + 'px', marginTop: -16 + 'px' }}>
+        <Typography variant='body1'>{blog.likes} likes</Typography>
+        {user && <Button variant='outlined' size='small' onClick={handleLike}>like</Button>}
+        {isCreator && <Button variant='outlined' size='small' color='error' onClick={handleRemove}>remove</Button>}
+      </CardActions>
+    </Card>
   )
 }
 
