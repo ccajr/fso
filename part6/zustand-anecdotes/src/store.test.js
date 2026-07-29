e6b68dc6d539f5cc1025584282bfb6fc06bb758a
Cathlyn Clyde Resmeros <cathlyn.jandog@gmail.com>
2026-07-29 22:25:26 +0800

    Add exercise 6.13 solution

--- part6/zustand-anecdotes/src/store.test.js
+++ part6/zustand-anecdotes/src/store.test.js
@@ -29,4 +29,25 @@ describe('useAnecdoteActions', () => {
 
     expect(useAnecdoteStore.getState().anecdotes).toEqual(mockAnecdotes)
   })
+
+  it('component displaying anecdotes receives the anecdotes from the store sorted by votes', async () => {
+    const mockAnecdotes = [
+      { id: 1, content: 'Third based on votes', votes: 2 },
+      { id: 2, content: 'First', votes: 23 },
+      { id: 3, content: 'Second', votes: 8 }
+    ]
+    const sortedMockAnecdotes = [
+      mockAnecdotes[1], mockAnecdotes[2], mockAnecdotes[0]
+    ]
+    anecdoteService.getAll.mockResolvedValue(mockAnecdotes)
+
+    const { result } = renderHook(() => useAnecdoteActions())
+
+    await act(async () => {
+      await result.current.initialize()
+    })
+
+    const { result: anecdotesResult } = renderHook(() => useAnecdotes())
+    expect(anecdotesResult.current).toEqual(sortedMockAnecdotes)
+  })
 })
